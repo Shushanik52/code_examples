724411883c48c3383bcbfc4380b7156ed55285a4
Shushan <shushan.vardanyan@esterox.am>
2022-07-25 12:00:56 +0400

    vue example

---
+++ Vue/Example2/loaderComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="loader" v-if="loader">
+    <img src="/img/loading.gif" alt="template-badge">
+  </div>
+</template>
+<script>
+export default {
+  name: 'pic-snippet-loader',
+  data() {
+    return {
+      loader: false,
+    }
+  },
+  mounted() {
+    Bus.$on('showLoading', (data) => {
+      if (data) {
+        this.loader = true;
+      } else {
+        this.loader = false;
+      }
+    });
+  },
+  methods: {},
+  destroyed() {
+    Bus.$off('showLoading');
+  }
+}
+</script>
+<style lang="scss" scoped>
+.loader {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(#F3FBFE, .5);
+  z-index: 99999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+Footer
